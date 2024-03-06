17216ea94de1a2c2f25c7f440eed43721dc3a648
BOGA SHRAVANI <shravaniboga07@gmail.com>
2024-03-07 00:51:52 +0530

    changed deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  // Other Vite configuration options...
+  server: {
+    // Configure MIME types
+    mimeTypes: {
+      // Add or modify the MIME type for JavaScript files
+      ".js": "application/javascript",
+    },
+  },
+});
